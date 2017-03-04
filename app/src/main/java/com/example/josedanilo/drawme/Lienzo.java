22bd1e340e4556b7651e0de00eb3719513d78999
Jose Vallecillo <jose.vallecillo@ujcv.edu.hn>
2017-03-04 10:54:53 -0600

    Creando clase Lienzo, es sobre la que se va pintar.

---
+++ app/src/main/java/com/example/josedanilo/drawme/Lienzo.java
@@ -0,0 +1,15 @@
+package com.example.josedanilo.drawme;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jose Danilo on 04/03/2017.
+ */
+
+public class Lienzo extends View {
+    public Lienzo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
